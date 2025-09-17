f3e573d9eb6cd10c1e4b04afc93ea004527f666b
Lieto-mieli <lieto.malmiola@outlook.com>
2025-09-17 11:17:59 +0300

    Translated code to English

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import mysql.connector
 import geopy
 from geopy import distance
-def sqlhaku(query):
-    kursori = yhteys.cursor()
-    kursori.execute(query)
-    tulos = kursori.fetchone()
-    return tulos
+def sqlquery(query):
+    cursor = sqlconnection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return result
 def valimatka(icao1, icao2):
-    location1 = sqlhaku(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{icao1}'")
-    location2 = sqlhaku(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{icao2}'")
+    location1 = sqlquery(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{icao1}'")
+    location2 = sqlquery(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{icao2}'")
     return geopy.distance.distance(location1, location2)
-yhteys = mysql.connector.connect(
+sqlconnection = mysql.connector.connect(
     host='localhost',
     port='3306',
     database='flight_game',
-    user='Lieto',
-    password='test',
+    user='',
+    password='',
     autocommit=True
 )
 #firstICAO = input("Enter the ICAO code of the first airport: ")
