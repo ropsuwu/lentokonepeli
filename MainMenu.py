45bd523430d00dce0018c2bfddcf8cab2a495f4f
Lieto-mieli <lieto.malmiola@outlook.com>
2025-09-25 14:01:13 +0300

    fix

--- MainMenu.py
+++ MainMenu.py
@@ -12,20 +12,20 @@ menuState = "main"
 while True:
     if menuState == "main":
         ans = input("Please input '1' to start a new game, '2' to view highscores, or '3' to exit the game")
-        if ans == 1:
+        if ans == "1":
             #start the game
             GameLoop.game()
-        elif ans == 2:
+        elif ans == "2":
             #switch to highscore menu
             printscores()
             menuState = "scores"
-        elif ans == 3:
+        elif ans == "3":
             quit()
         else:
             "ERROR; Not a valid input."
     elif menuState == "scores":
         ans = input(f"input '1' to return to the mainmenu")
-        if ans == 1:
+        if ans == "1":
             menuState = "main"
         else:
             "ERROR; Not a valid input."
