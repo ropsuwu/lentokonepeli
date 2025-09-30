6d1fb77340a78e1a13173dfb3ab63665fea75f17
Lieto-mieli <lieto.malmiola@outlook.com>
2025-09-30 12:35:27 +0300

    stashed changes
    github stuff

--- MainMenu.py
+++ MainMenu.py
@@ -6,6 +6,7 @@ def printscores():
         print(f"{sqlresult[i]}")
 
 menuState = "main"
+<<<<<<< Updated upstream
 mods = {
     "2hearted": False,
     "estart": False,
@@ -13,19 +14,28 @@ mods = {
     "dyslexia": False,
 }
 cbmodes = ["Off", "Protanopia", "Deuteranopia", "Tritanopia"]
+=======
+>>>>>>> Stashed changes
 #expected values for 'menuState' are 'main', 'scores', and 'mods'
 
 while True:
     if menuState == "main":
+<<<<<<< Updated upstream
         ans = input("(1) Start a new game\n"
                     "(2) Change game modifiers\n"
                     "(3) View highscores\n"
                     "(4) Exit the game\n>")
+=======
+        ans = input("(1) Start a new game\n(2) Change game modifiers\n(3) View highscores\n(4) Exit the game\n> ")
+>>>>>>> Stashed changes
         if ans == "1":
             #start the game
             GameLoop.game(mods)
         elif ans == "2":
+<<<<<<< Updated upstream
             #switch to modifiers menu
+=======
+>>>>>>> Stashed changes
             menuState = "mods"
         elif ans == "3":
             #switch to highscore menu
@@ -35,6 +45,7 @@ while True:
         else:
             print("ERROR; Not a valid input.")
     elif menuState == "mods":
+<<<<<<< Updated upstream
         ans = input(f"(1) Two hearts: {mods['2hearted']}\n"
                     f"(2) Extreme start: {mods['estart']}\n"
                     #a 'daily run' type modifier could work here
@@ -52,6 +63,9 @@ while True:
             mods["colourblind"] = cbmodes[(cbmodes.index(mods["colourblind"])+1)%4]
         elif ans == "9":
             mods["dyslexia"] = not mods["dyslexia"]
+=======
+        print(f"")
+>>>>>>> Stashed changes
     elif menuState == "scores":
         printscores()
         ans = input(f"Return to the main-menu by pressing enter")
